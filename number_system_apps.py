62ce3ac07afb3baa66326c3e4b76fa001a9d3299
liotrusy <pierre.collura1989@gmail.com>
2020-12-30 13:43:45 +0100

    implemented check for no double symbols in the game functionality

--- number_system_apps.py
+++ number_system_apps.py
@@ -1,23 +1,5 @@
 import conversion_logic as conv
 
-def correct_guess_checker(value_map, number_input, correct_guess):
-
-    if set(number_input) != set(value_map.keys()):
-        print("You have used characters not included in your number system")
-        return "wrong characters"
-
-    number_int = conv.convert_to_int(number_input, value_map)
-    result = number_int == correct_guess
-
-    if result:
-        print(f"Yes, the number {number_input} is equal to {correct_guess}.")
-    else:
-        print(f"Sorry, the number {number_input} is not equal to {correct_guess}, it is actually {number_int}")
-        print("Here is a map of your number system")
-        print(value_map)
-    
-    return result
-
 # starting message
 print("""
 This application helps you create and play around with you own number system. Let's get started!
@@ -33,48 +15,71 @@ So, to create this number system you'd have to type below: g,a,m,e.
 Now, it's your turn! Create your own number system.
 Insert below the symbols here - remember start from 0, and separate the symbols with a comma (,).""")
 
-# capture inputs from player
+# users inserts inputs for new number system
 symbols = input(">> ")
 
-# ensure that input is not empty
+# users cannot continue playing until they insert a set of symbols
 while not symbols:
     print("\nWhat, no input?! We need symbols to create your number system! Please insert the symbols you'd like to use. (Otherwise, no hard feelings. Enter CTRL + D to interrupt the game)")
     symbols = input(">> ")
 
-# ensure that the symbol list includese more 2 or more characters
+# useres cannot continue playing until the set of symbols contains 2 or more elements
 symbol_list = conv.clean_input(symbols)
 while len(symbol_list) < 2:
     print("\nOops.. you must use 2 or more symbols to create for your own number system! Please insert a new set of symbols. (If you want to stop playing enter CTRL + D to interrupt the game)")
     symbols = input(">> ")
     symbol_list = conv.clean_input(symbols)
 
-# prevent player to move on if inputs are not unique symbols
+# users cannot continue playing until the symbols inserted are not unique
 while conv.check_for_no_duplicates(symbol_list):
     print("\nATTENTION: You cannot have duplicates symbols of your number system! Otherwise how can you tell the difference between two numbers with the same symbol.")
     print("Just go ahead and insert a new set of symbols (Or, if you want to stop the game enter CTRL + D)")
     symbols = input(">> ")
     symbol_list = conv.clean_input(symbols)
 
-# create a value map
+# users cannot continue playing until the symbols inserted are not unique
+while conv.check_for_no_double_symbols(symbol_list):
+    print("\nWatch out! Your base numbers can be represented by only one symbol - no doubles allowed.")
+    print("Just go ahead and insert a new set of symbols (Or, if you want to stop the game enter CTRL + D)")
+    symbols = input(">> ")
+    symbol_list = conv.clean_input(symbols)
+
+# user can see the value map and can begin playing
 value_map = conv.create_value_map(symbol_list)
 print(f"""
 
 Great you have created your number system. Here is the map of your number system:
 {value_map}
 
-Now, we can start playing. Let's see if you can create a number and guess it correctly.
-Type a number below using your newly created number system!""")
+Now, we can start playing. Let's see if you can create a number and guess it correctly.""")
 
-# again = "y"
-# while again == "y":
-#     number_input = input("Type your number here: ")
-#     correct_guess = int(input("So, what number was it? "))
-#     correct_guess_checker(value_map, number_input, correct_guess)
-#     again = input("\nDo you want to play again? [y/n]")
+# user guesses if the number inserted is correct until decides not to play again
+def correct_guess_checker(value_map, number_input, correct_guess):
+
+    for symbol in number_input:
+        if not (symbol in value_map.keys()):
+            print("You have used characters not included in your number system")
+            return "wrong characters"
+
+    number_int = conv.convert_to_int(number_input, value_map)
+    result = number_int == correct_guess
+
+    if result:
+        print(f"Yes, the symbol '{number_input}' is equal to {correct_guess}.")
+    else:
+        print(f"Sorry, the symbol '{number_input}' is not equal to the number {correct_guess}, it actually represents the number {number_int}")
+        print("Here is a reminder of the map of your number system")
+        print(value_map)
+    
+    return result
 
 
-# print("Thanks for playing! See you next time")
+again = "y"
+while again == "y":
+    number_input = input("Type a number using your number system here: ")
+    correct_guess = int(input("So, what number was it? "))
+    correct_guess_checker(value_map, number_input, correct_guess)
+    again = input("\nDo you want to play again? [y/n]")
 
-
-
-
+# user sees thank you message
+print("Thanks for playing! See you next time")
